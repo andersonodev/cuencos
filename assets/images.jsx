328d39a8c22318495f38afb9ff073e1db3fd6e44
Anderson Lima ☕ <72580323+andersonodev@users.noreply.github.com>
2025-05-24 09:18:09 -0300

    Updates

---
+++ assets/images.jsx
@@ -0,0 +1,23 @@
+import logoPreta from './logo-preta.png';
+// Descomente e importe as outras imagens conforme necessário
+// import logoRoxa from './logo-roxa.png';
+// import logoCuencosRoxa from './logo-cuencos-roxa.png';
+// import logoCuencosPreta from './logo-cuencos-preta.png';
+// import logoCuencosBranca from './logo-cuencos-branca.png';
+// import dashboardIcon from './dashboard-icon.png';
+// import ticketIcon from './ticket.png';
+// import profileIcon from './profile.png';
+// import starIcon from './star.png';
+
+export const images = {
+  logoPreta,
+  // Descomente conforme for importando as imagens acima
+  // logoRoxa,
+  // logoCuencosRoxa,
+  // logoCuencosPreta,
+  // logoCuencosBranca,
+  // dashboardIcon,
+  // ticketIcon,
+  // profileIcon,
+  // starIcon,
+};
